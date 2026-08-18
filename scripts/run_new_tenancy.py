bcb7f2c4dcf498649c2f7c01e48567e3aeffe6f2
Varsiha <varsiha.sothilingam@ncas.ac.uk>
2026-08-18 10:15:15 +0100

    updated with new script

--- scripts/run_new_tenancy.py
+++ scripts/run_new_tenancy.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import sys
 from activestorage.active import Active
+import argparse
 
 S3_BUCKET = "bnl"
 
@@ -9,23 +10,22 @@ def test_s3_file(filename, variable, sample_slice):
     """
     Run the test with parameterized file, variable, and slicing via s3
     """
-    print("Check 1 ")
     storage_options = {
         'key': "f2d55c6dcfc7618b2c34e00b58df3cef",
         'secret': "$/'#M{0{/4rVhp%n^(XeX$q@y#&(NM3W1->~N.Q6VP.5[@bLpi='nt]AfH)>78pT",
         'client_kwargs': {'endpoint_url': "https://uor-aces-o.s3-ext.jc.rl.ac.uk"},  # final proxy
     }
-    print("Check 2 ")
+
     # Reductionist on Wacasoft (behind firewall)
     active_storage_url = "https://reductionist.jasmin.ac.uk/" 
     #active_storage_url = "https://wacasoft1.jasmin.ac.uk/"
 
     # Construct the URI using the passed filename
     test_file_uri = os.path.join(S3_BUCKET,filename)
-    print("Check 3 ")
+    
     print(f"S3 Test file path: {test_file_uri}")
     print(f"Target Variable: {variable}")
-    print("Check 4 ")
+
     active = Active(
         test_file_uri, 
         variable, 
@@ -34,10 +34,9 @@ def test_s3_file(filename, variable, sample_slice):
         active_storage_url=active_storage_url,
         option_disable_chunk_cache=False
     )
-    print("Check 5 ")
+
     active._version = 2
     active._method = "min"
-    active._max_threads = 100
 
     result = active[sample_slice]
     #result = active.mean(axis=(1,2))[sample_slice]
@@ -46,20 +45,21 @@ def test_s3_file(filename, variable, sample_slice):
     return result
 
 
-def test_https_file():
+def test_https_file(maxThreads, slice):
     """
     Run the test with parameterized file, variable, and slicing via https
     """
     active_storage_url = "https://reductionist.jasmin.ac.uk/"
-    test_file_uri = "https://esgf.ceda.ac.uk/thredds/fileServer/esg_cmip6/CMIP6/AerChemMIP/MOHC/UKESM1-0-LL/ssp370SST-lowNTCF/r1i1p1f2/Amon/cl/gn/v20200420/cl_Amon_UKESM1-0-LL_ssp370SST-lowNTCF_r1i1p1f2_gn_205001-209912.nc"
+    #test_file_uri = "https://esgf.ceda.ac.uk/thredds/fileServer/esg_cmip6/CMIP6/AerChemMIP/MOHC/UKESM1-0-LL/ssp370SST-lowNTCF/r1i1p1f2/Amon/cl/gn/v20200420/cl_Amon_UKESM1-0-LL_ssp370SST-lowNTCF_r1i1p1f2_gn_205001-209912.nc"
+    test_file_uri = "https://gws-access.jasmin.ac.uk/public/canari/varsiha/cl_Amon_UKESM1-0-LL_ssp370SST-lowNTCF_r1i1p1f2_gn_205001-209912.nc"
     active = Active(test_file_uri, ncvar="cl",  active_storage_url=active_storage_url, option_disable_chunk_cache=True)
     active._version = 2
     active._method = "min"
-    active._max_threads = 100
+    active._max_threads = maxThreads
   
     #result = active[0:40, 0:1920, 0:30, 0:30][0]
     #dataset cl: 600, 85, 144, 192
-    result = active[0:40,:,:,:]#0:600,0:85,0:144,0:192]
+    result = active[slice:slice+20,:,:,:]#0:600,0:85,0:144,0:192]
     print(result)
 
     return result
@@ -72,7 +72,7 @@ if __name__ == "__main__":
     #----------------------------------------------------
 
     # Check if we got the right number of arguments
-    
+    """
     if len(sys.argv) < 4:
         print("Usage: python script.py <file> <var> <slices_string>")
         sys.exit(1)
@@ -87,9 +87,30 @@ if __name__ == "__main__":
                           for i in range(0, len(slice_data), 2))
 
     final_result = test_s3_file(target_file, target_var, active_slices)
-    
+    """
     #----------------------------------------------------
     #  https access- don't bother with the runTesting.sh, just do it manually
     #----------------------------------------------------  
 
-    #test_https_file()
+    parser = argparse.ArgumentParser(
+        description="To add parser options for LOTUS Batch submission"
+    )
+
+    parser.add_argument(
+        "--maxThreads",
+        nargs=1,          # exactly 3 values
+        type=int,         # convert them to integers
+        help="Value for maximum number of threads"
+    )
+    parser.add_argument(
+        "--slice",
+        nargs=1,          # exactly 3 values
+        type=int,         # convert them to integers
+        help="Value for maximum number of threads"
+    )
+    args = parser.parse_args()
+
+    print(args.maxThreads[0], args.slice[0])
+
+
+    test_https_file(args.maxThreads[0], args.slice[0])
